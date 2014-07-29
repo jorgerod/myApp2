99c6259052465d24ed7744f8689e55757431be31
Jorge Rodriguez <jorgerod84@gmail.com>
2014-07-29 19:36:16 +0200

    Fixme: problemas de versiones

---
+++ src/main/java/com/pimpos/app/App.java
@@ -0,0 +1,23 @@
+package com.pimpos.app;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request arg0, Response arg1) {
+                return "Hello nurcio";
+            }
+        });
+
+    }
+
+}
